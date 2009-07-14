72a910577723b620bedfe6718e5897b11a15f1e2
(no author) <(no author)@8a9318a1-56ba-4d59-b755-99d26321be01>
2009-07-14 16:49:45 +0000

    reorganized statistics display and added several additional statistics, also added descriptions to doc dictionary
    git-svn-id: https://svn.ssec.wisc.edu/repos/glance/trunk@19 8a9318a1-56ba-4d59-b755-99d26321be01

--- pyglance/glance/delta.py
+++ pyglance/glance/delta.py
@@ -124,63 +124,217 @@ def _get_num_perfect(a, b, ignoreMask=None):
         numPerfect = sum(a == b)
     return numPerfect
 
-def summarize(a, b, epsilon=0., (amiss,bmiss)=(None,None), ignoreMask=None):
-    """return dictionary of similarity statistics
+def _get_nan_stats(a, b) :
+    """
+    Get a list of statistics about non-numerical values in data sets a and b,
+    the return value will be a dictionary of statistics
+    """
+    # find the nan values in the data
+    a_nans = isnan(a)
+    num_a_nans = sum(a_nans)
+    b_nans = isnan(b)
+    num_b_nans = sum(b_nans)
+    num_common_nans = sum(a_nans & b_nans)
+    
+    # make the assumption that a and b are the same size and only use the size of a
+    total_num_values = a.size
+    
+    nan_stats = {'a_nan_count': num_a_nans,
+                 'a_nan_fraction': (float(num_a_nans) / float(total_num_values)),
+                 'b_nan_count': num_b_nans,
+                 'b_nan_fraction': (float(num_b_nans) / float(total_num_values)),
+                 'common_nan_count': num_common_nans,
+                 'common_nan_fraction': (float(num_common_nans) / float(total_num_values))
+                 }
+    
+    return nan_stats
+    
+def _get_missing_value_stats(a_missing_mask, b_missing_mask) :
+    """
+    Get a list of statistics about missing data values in data sets a and b,
+    given masks describing the positions of the missing data (such masks
+    can be gotten from the diff function), 
+    the return value will be a dictionary of statistics
+    """
+    # calculate information about the missing values
+    num_a_missing = sum(a_missing_mask)
+    num_b_missing = sum(b_missing_mask)
+    num_common_missing = sum(a_missing_mask & b_missing_mask)
+    
+    # make the assumption that a and b are the same size and only use the size of a's mask
+    total_num_values = a_missing_mask.size
+    
+    missing_value_stats = {'a_missing_count': num_a_missing,
+                           'a_missing_fraction': (float(num_a_missing) / float(total_num_values)),
+                           'b_missing_count': num_b_missing,
+                           'b_missing_fraction': (float(num_b_missing) / float(total_num_values)),
+                           'common_missing_count': num_common_missing,
+                           'common_missing_fraction': (float(num_common_missing) / float(total_num_values))
+                           }
+    
+    return missing_value_stats
+    
+def _get_finite_data_stats(a_is_finite_mask, b_is_finite_mask) :
+    """
+    Get a list of statistics about finite data values in data sets a and b,
+    given masks describing the positions of the finite data in each file (such masks
+    can be gotten from the diff function),
+    the return value will be a dictionary of statistics
+    """
+    # calculate information about the finite data
+    num_a_finite = sum(a_is_finite_mask)
+    num_b_finite = sum(b_is_finite_mask)
+    num_common_finite = sum(a_is_finite_mask & b_is_finite_mask)
+    num_finite_in_only_one = sum(a_is_finite_mask ^ b_is_finite_mask) # use an exclusive OR
+    
+    # make the assumption that a and b are the same size and only use the size of a's mask
+    total_num_values = a_is_finite_mask.size
+    
+    finite_value_stats = {'a_finite_count': num_a_finite,
+                          'a_finite_fraction': (float(num_a_finite) / float(total_num_values)),
+                          'b_finite_count': num_b_finite,
+                          'b_finite_fraction': (float(num_b_finite) / float(total_num_values)),
+                          'common_finite_count': num_common_finite,
+                          'common_finite_fraction': (float(num_common_finite) / float(total_num_values)),
+                          'finite_in_only_one_count': num_finite_in_only_one,
+                          'finite_in_only_one_fraction': (float(num_finite_in_only_one) / float(total_num_values)),
+                          }
+    
+    return finite_value_stats
+
+def _get_general_data_stats(a_missing_value, b_missing_value, epsilon, trouble_mask) :
+    """
+    Get a list of general statistics about a and b, given a and b and some other information
+    about them.
+    the return value will be a dictionary of statistics
+    """
+    # figure out how much trouble we had
+    num_trouble = sum(trouble_mask)
+    
+    # make the assumption that a and b are the same size/shape as their trouble mask
+    total_num_values = trouble_mask.size
+    
+    general_stats = {'a_missing_value': a_missing_value,
+                     'b_missing_value': b_missing_value,
+                     'epsilon': epsilon,
+                     'num_data_points': total_num_values,
+                     'shape': trouble_mask.shape,
+                     'trouble_points_count': num_trouble,
+                     'trouble_points_fraction': num_trouble/ float(total_num_values)
+                     }
+    
+    return general_stats
+
+def _get_numerical_data_stats(a, b, diff_data, data_is_finite_mask, epsilon, additional_statistics={}) :
+    """
+    Get a list of numerical comparison related statistics about a and b,
+    given a and b and some other information about them.
+    the return value will be a dictionary of statistics
+    """
+    # calculate our various statistics
+    num_finite_values_too_different = sum(abs(diff_data[data_is_finite_mask]) > epsilon)
+    num_perfect = _get_num_perfect(a, b, ~data_is_finite_mask)
+    r_corr = corr(a, b, data_is_finite_mask)
+    
+    # we actually want the total number of _finite_ values rather than all the data
+    total_num_finite_values = sum(data_is_finite_mask)
+    
+    comparison = {  'diff_outside_epsilon_count': num_finite_values_too_different,
+                    'diff_outside_epsilon_fraction': num_finite_values_too_different / float(total_num_finite_values),
+                    'perfect_match_count': num_perfect,
+                    'perfect_match_fraction': num_perfect / float(total_num_finite_values),
+                    'correlation': r_corr
+                    }
+    comparison.update(additional_statistics)
+    
+    return comparison
+
+def summarize(a, b, epsilon=0., (a_missing_value, b_missing_value)=(None,None), ignoreMask=None):
+    """return dictionary of statistics dictionaries
     stats not including 'nan' in name exclude nans in either arrays
     """
-    d, mask, trouble, (anfin, bnfin), (amis, bmis) = nfo = diff(a,b,epsilon,(amiss,bmiss),ignoreMask)
-    a_xor_b_finite = sum(anfin ^ bnfin)
-    stadic = stats(*nfo)
-    n_o_e = sum(trouble)
-    a_nans = isnan(a)
-    b_nans = isnan(b)
-    n = a.size
-    r = corr(a,b,mask)
-    n_perfect = _get_num_perfect(a, b, ignoreMask)
-    f_perfect = n_perfect / float(n)
-        
-    out = { 'a_xor_b_finite_count': a_xor_b_finite,
-             'finite_count': sum(mask),
-             'a_missing_count': sum(amis),
-             'b_missing_count': sum(bmis),
-             'outside_epsilon_count': n_o_e,
-             'outside_epsilon_fraction': n_o_e / float(n),
-             'trouble_points_count': n_o_e,
-             'max_count': n,
-             'a_nan_count': sum(a_nans),
-             'b_nan_count': sum(b_nans),
-             'a_and_b_nan_count': sum(a_nans & b_nans),
-             'shape': a.shape,
-             'perfect_count': n_perfect,
-             'perfect_fraction': f_perfect,
-             'correlation': r
-             }
-    out.update(stadic)
+    d, mask, trouble, (anfin, bnfin), (amis, bmis) = nfo = diff(a,b,epsilon,(a_missing_value, b_missing_value),ignoreMask)
+    additional_statistics = stats(*nfo)
+    
+    # build some other finite data masks that we'll need
+    finite_a_mask = ~(anfin | amis)
+    finite_b_mask = ~(bnfin | bmis)
+    finite_mask = finite_a_mask & finite_b_mask
+    if not (ignoreMask is None) :
+        finite_a_mask = finite_a_mask & (~ ignoreMask)
+        finite_b_mask = finite_b_mask & (~ ignoreMask)
+        finite_mask = finite_mask & (~ ignoreMask)
+    
+    general_stats = _get_general_data_stats(a_missing_value, b_missing_value, epsilon, trouble)
+    comparison_stats = _get_numerical_data_stats(a, b, d, finite_mask, epsilon, additional_statistics)
+    nan_stats = _get_nan_stats(a, b)
+    missing_stats = _get_missing_value_stats(amis, bmis)
+    finite_stats = _get_finite_data_stats(finite_a_mask, finite_b_mask)
+    
+    out = {}
+    out['NaN Statistics'] = nan_stats
+    out['Missing Value Statistics'] = missing_stats
+    out['Finite Data Statistics'] = finite_stats
+    out['Numerical Comparison Statistics'] = comparison_stats
+    out['General Statistics'] = general_stats
+    
     return out
 
-STATISTICS_DOC = {  'general': "Finite values are non-missing and finite (not NaN or +-Inf)",
+STATISTICS_DOC = {  'general': "Finite values are non-missing and finite (not NaN or +-Inf); fractions are out of all data, " +
+                               "both finite and not, unless otherwise specified",
+                    
+                    # general statistics
+                    'a_missing_value': 'the value that is considered \"missing\" data when it is found in A',
+                    'b_missing_value': 'the value that is considered \"missing\" data when it is found in B',
+                    'epsilon': 'amount of difference between matching data points in A and B that is considered acceptable',
+                    'num_data_points': "number of data values in A",
+                    'shape': "shape of A",
+                    'trouble_points_count': 'number of points that are nonfinite or missing in either input data set (A or B),' +
+                                            ' or are unacceptable when compared (according to the current epsilon value)',
+                    'trouble_points_fraction': 'fraction of points that are nonfinite or missing in either input data set (A or B),' +
+                                               ' or are unacceptable when compared (according to the current epsilon value)',
+                    
+                    # finite data stats descriptions
+                    'a_finite_count': "number of finite values in A",
+                    'a_finite_fraction': "fraction of finite values in A (out of all data points in A)",
+                    'b_finite_count': "number of finite values in B",
+                    'b_finite_fraction': "fraction of finite values in B (out of all data points in B)",
+                    'common_finite_count': "number of finite values in common between A and B",
+                    'common_finite_fraction': "fraction of finite values in common between A and B",
+                    'finite_in_only_one_count': "number of values that changed finite-ness between A and B",
+                    'finite_in_only_one_fraction': "fraction of values that changed finite-ness between A and B",
+                    
+                    # missing data value statistics
+                    'a_missing_count': "number of values flagged missing in A",
+                    'a_missing_fraction': "fraction of values flagged missing in A",
+                    'b_missing_count': "number of values flagged missing in B",
+                    'b_missing_fraction': "fraction of values flagged missing in B",
+                    'common_missing_count': "number of missing values in common between A and B",
+                    'common_missing_fraction': "fraction of missing values in common between A and B",
+                    
+                    # NaN related statistics
+                    'a_nan_count': "number of NaNs in A",
+                    'a_nan_fraction': "fraction of NaNs in A",
+                    'b_nan_count': "number of NaNs in B",
+                    'b_nan_fraction': "fraction of NaNs in B",
+                    'common_nan_count': "number of NaNs in common between A and B",
+                    'common_nan_fraction': "fraction of NaNs in common between A and B",
+                    
+                    # Numerical comparison statistics
+                    'correlation': "Pearson correlation r-coefficient (0.0-1.0) for finite values of A and B",
+                    'diff_outside_epsilon_count': "number of finite differences falling outside epsilon",
+                    'diff_outside_epsilon_fraction': "fraction of finite differences falling outside epsilon (out of common_finite_count)",
+                    'mean_diff': "mean difference of finite values",
+                    'median_diff': "median difference of finite values",
+                    'perfect_match_count': "number of perfectly matched finite data points between A and B",
+                    'perfect_match_fraction': "fraction of finite values perfectly matching between A and B (out of common_finite_count)",
+                    'rms_diff': "root mean square (RMS) difference of finite values",
+                    'std_diff': "standard deviation of difference of finite values",
+                    
+                    # note: the statistics described below may no longer be generated?
                     'mean_percent_change': "Percent change from A to B for finite values, averaged",
                     'max_percent_change': "Percent change from A to B for finite values, maximum value",
-                    'a_xor_b_finite_count': "number of values that changed finite-ness between A and B",
-                    'mean_diff': "Mean difference of finite values",
-                    'std_diff': "Stdev of difference of finite values",
                     'max_diff': "Maximum difference of finite values",
-                    'median_diff': "Median difference of finite values",
-                    'rms_diff': "RMS difference of finite values",
-                    'finite_count': "number of finite values in common between A and B",
-                    'outside_epsilon_count': "number of finite differences falling outside epsilon",
-                    'outside_epsilon_fraction': "fraction of values falling outside epsilon (outside_epsilon_count/max_count)",
-                    'trouble_points_count': "number of pairs of data points in A and B with mismatching finite-ness or a difference falling outside epsilon",
-                    'max_count': "number of values (cumprod(shape))",
-                    'a_missing_count': "number of values flagged missing in A",
-                    'b_missing_count': "number of values flagged missing in B",
-                    'a_nan_count': "number of NaNs in A",
-                    'b_nan_count': "number of NaNs in B",
-                    'a_and_b_nan_count': "number of NaNs in common between A and B",
-                    'perfect_count': "number of perfectly matched data points between A and B",
-                    'perfect_fraction': "fraction of values perfectly matching between A and B",
-                    'correlation': "Pearson correlation r-coefficient (0.0-1.0) for finite values of A and B",
-                    'shape': "shape of A"
                     }
 STATISTICS_DOC_STR = '\n'.join( '%s:\n    %s' % x for x in sorted(list(STATISTICS_DOC.items())) ) + '\n'
 
