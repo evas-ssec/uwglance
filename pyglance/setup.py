8377dca7349ab1ce79b8ae77603342eb9078744b
(no author) <(no author)@8a9318a1-56ba-4d59-b755-99d26321be01>
2010-06-01 22:13:31 +0000

    added comment to setup.py and changed version number
    git-svn-id: https://svn.ssec.wisc.edu/repos/glance/trunk@117 8a9318a1-56ba-4d59-b755-99d26321be01

--- pyglance/setup.py
+++ pyglance/setup.py
@@ -3,6 +3,10 @@
 $Id: setup.py 66 2009-04-24 18:47:55Z rayg $
 see http://peak.telecommunity.com/DevCenter/setuptools
 
+note: if you want to develop this code and run from code on the command line,
+please run the following line when you update to a new version of the code.
+python setup.py develop --install-dir=$HOME/Library/Python
+
 distribution:
 python setup.py develop --install-dir=$HOME/Library/Python
 python setup.py sdist
@@ -18,7 +22,7 @@ easy_install -d $HOME/Library/Python -vi http://larch.ssec.wisc.edu/eggs/repos g
 from setuptools import setup, find_packages
 
 setup( name="glance", 
-       version="0.2.6.17", 
+       version="0.2.6.18", 
        zip_safe = True,
        entry_points = { 'console_scripts': [ 'glance = glance.compare:main' ] },
        packages = find_packages('.'),
