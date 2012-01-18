e45f5ad5db06ec832cdf198aa19b0f6baa911989
(no author) <(no author)@8a9318a1-56ba-4d59-b755-99d26321be01>
2012-01-18 20:42:47 +0000

    changing version number so I can release
    git-svn-id: https://svn.ssec.wisc.edu/repos/glance/trunk@163 8a9318a1-56ba-4d59-b755-99d26321be01

--- pyglance/setup.py
+++ pyglance/setup.py
@@ -22,7 +22,7 @@ easy_install -d $HOME/Library/Python -vi http://larch.ssec.wisc.edu/eggs/repos g
 from setuptools import setup, find_packages
 
 setup( name="glance", 
-       version="0.2.7.02", 
+       version="0.2.7.03", 
        zip_safe = False,
        entry_points = { 'console_scripts': [ 'glance = glance.compare:main' ] },
        packages = ['glance'], #find_packages('.'),
