fixing variable sync in the gui and making a preliminary change to improve statistic display precision

pyglance/glance/report.py
@@ -21,7 +21,7 @@ from glance.constants import *
 LOG = logging.getLogger(__name__)
 
 # TODO, this should be overridable in the config file when there is one
-floatFormat = '%#0.4g'
+floatFormat = '%#0.8g'
 formattingSettings = {
                       types.FloatType: floatFormat,
                       np.float32: floatFormat,